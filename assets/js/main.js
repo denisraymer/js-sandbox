a5a03f8a8663d47f65da5b979a1fa778be60e3bd
denisraimer <to.mars.66.miles@gmail.com>
2020-02-28 18:11:36 +0300

    Тернарный оператор
    условие ? если условие true : если условие false

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,25 @@
 function mainFunction() {
-    // Object
+    // Тернарный оператор
     const user = {
         name: 'Denis',
+        statusAdmin: false,
         age: 22
     };
+    let colorPallet = ['Black', 'Blue', 'Green', 'Yellow', 'Pink', 'Purple'];
     let value;
 
-    // .hasOwnProperty('prop') - проверяет на наличие свойства не проверяет на наличие значения
-    if (user.hasOwnProperty('name')) {
-        value = user.name;
-    } else {
-        value = 'false';
-    }
+    value = user.statusAdmin ? !user.statusAdmin : 'No access';
 
-    // || - приобразовывает к true,
-    // && - приобразовывает к false
-
-    value = 1 || 0; // true
-    value = 1 && 0; // false
-
-    if (user.age >= 18 || user.age >= 21) {
-        value = 'Some actions';
-    } else {
-        value = 'No actions';
-    }
-
-    // Присвоение значения переменной
-    value = user.name || 'Den';
-    value = 1 && 0 && 2 && 3;
-
-    if (user.age >= 21 && user.age <= 65) {
-        value = 'Some actions';
-    } else {
-        value = 'No actions';
+    switch (colorPallet[0]) {
+        case 'Black':
+            value = 'It the Black color';
+            break;
+        case 'Blue':
+        case 'Green':
+            value = 'It the Blue and Greens colors';
+            break;
+        default:
+            value = 'There is no color';
     }
 
     console.log(value);
