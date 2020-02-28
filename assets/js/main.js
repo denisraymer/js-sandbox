bc79b19a7809c271ede8dea1c9f039341ab9b922
denisraimer <to.mars.66.miles@gmail.com>
2020-02-28 17:29:07 +0300

    Логические операторы
    Оператор - это символ или ключевое слово, которое производит вычисление над некоторыми значениями. Эти значения называют операндами.
    
    Оператор с одним операндом называется унарным, с двумя - бинарным, с тремя - тернарным.
    
    Унарный: -10, +’10’
    Бинарный: 1 - 4, year = 2018
    Тернарный: 10 > 9 ? true : false
    
    1. В JavaScript есть логические значения true (истина) и false (ложь). Операторы сравнения возвращают их.
    2. Строки сравниваются побуквенно.
    3. Значения разных типов приводятся к числу при сравнении, за исключением строгого равенства === (!==).
    4. Значения null и undefined равны == друг другу и не равны ничему другому.
    5. Будьте осторожны при использовании сравнений типа > или < с переменными, которые иногда могут быть null/undefined. Сделать отдельную проверку для null/undefined – хорошая идея.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,41 @@
 function mainFunction() {
     // Object
     const user = {
-        firstName: 'Denis',
-        age: '18',
-        isAdmin: true,
-        email: 'test@gmail.com',
-        'user-address': {
-            city: 'Moscow'
-        },
-        skills: ['Python', 'C++', 'C#']
+        name: 'Denis',
+        age: 22
     };
-
     let value;
-    let prop = 'skills';
 
-    value = user.firstName;
-    value = user['isAdmin'];
-    value = user['user-address'];
-    value = user['user-address'].city;
-    value = user['user-address']['city'];
-    value = user['skills'][1];
+    // .hasOwnProperty('prop') - проверяет на наличие свойства не проверяет на наличие значения
+    if (user.hasOwnProperty('name')) {
+        value = user.name;
+    } else {
+        value = 'false';
+    }
 
-    value = user.firstName = 'Den';
-    value = user.info = 'Some info';
+    // || - приобразовывает к true,
+    // && - приобразовывает к false
 
-    value = user['user-address'].city = 'SPB';
-    value = user['user-address'].country = 'Russia';
+    value = 1 || 0; // true
+    value = 1 && 0; // false
+
+    if (user.age >= 18 || user.age >= 21) {
+        value = 'Some actions';
+    } else {
+        value = 'No actions';
+    }
+
+    // Присвоение значения переменной
+    value = user.name || 'Den';
+    value = 1 && 0 && 2 && 3;
+
+    if (user.age >= 21 && user.age <= 65) {
+        value = 'Some actions';
+    } else {
+        value = 'No actions';
+    }
 
     console.log(value);
-    console.log(user);
 }
 
 mainFunction();
