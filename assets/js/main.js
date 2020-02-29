4af933f198b152b2f52000eb44c0729f577b4fda
denisraimer <to.mars.66.miles@gmail.com>
2020-02-29 20:25:57 +0300

    Методы массивов
    Array.isArray(arr) - Проверка на массив это или нет
    .indexOf('Yellow') - Получение индекса нискомого элемента
    .push('Orange') - Добавляет в конец
    .pop() - Удаляет последниэ элемент
    .unshift('Orange') - Добавляет в начало
    .shift() - Удаляет начальный элемент
    .slice(0, 2) - Вырезает по индексу
    .splice(0, 2, 'Grey', 'White') - Удаляет и возвращет удаленные элементы, добавляет на место удаленных новое значение
    .concat(value) - Объединяет массив
    .concat(value) - Объединяет массив
    .join('-') - Преобразует в строку, разделяет по символьно
    .split(' ') - Создает массив, разделяет по символьно

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,38 @@
-// Функции
-
+// Методы массивов
 const user = {
     name: 'Denis',
     statusAdmin: false,
     age: 22
 };
-let colorPallet = ['Black', 'Blue', 'Green', 'Yellow', 'Pink', 'Purple'];
-let value;
 
-// Function declaration
-function userAuthentication(name, status = false) {
-    if (name === 'admin') {
-        return !status
-    }
+const mainFunction = function() {
+    let colorPallet = ['Black', 'Blue', 'Green', 'Yellow', 'Pink', 'Purple'];
+    let value;
 
-    return console.error('Access is denied !')
-}
+    value = colorPallet.length;
+    // colorPallet.length = 0;
+    // colorPallet.length = 100;
 
-value = userAuthentication('admin');
+    value = Array.isArray(colorPallet); // Проверка на массив это или нет
+    value = colorPallet[2] = 'Greens';
 
-value !== undefined ? console.log(value) : '';
+    value = colorPallet.indexOf('Yellow'); // Получение индекса нискомого элемента
+    value = colorPallet[value]; // Получение значения по индексу
 
-// Function expression
-const square = function (x) {
-    return x * x
+    value = colorPallet.push('Orange'); // Добавляет в конец
+    value = colorPallet.pop(); // Удаляет последниэ элемент
+    value = colorPallet.unshift('Orange'); // Добавляет в начало
+    value = colorPallet.shift(); // Удаляет начальный элемент
+    value = colorPallet.slice(0, 2); // Вырезает по индексу
+    value = colorPallet.splice(0, 2, 'Grey', 'White'); // Удаляет и возвращет удаленные элементы, добавляет на место удаленных новое значение
+    value = colorPallet.reverse();
+    value = colorPallet.concat(value); // Объединяет массив
+    value = [].concat(value); // Объединяет массив
+    value = colorPallet.join('-'); // Преобразует в строку, разделяет по символьно
+    value = 'Hello world'.split(' '); // Создает массив, разделяет по символьно
+
+    console.log(colorPallet);
+    console.log(value);
 };
 
-//
-(function (msg) {
-    console.log(msg)
-})('Hello world');
+mainFunction();
