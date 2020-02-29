68aae6d74869b2297715e2669f89879c990c9350
denisraimer <to.mars.66.miles@gmail.com>
2020-02-29 18:34:01 +0300

    Циклы
    break - останавливает цикл
    continue - прерывает текущую итерацию и переходит к следующей
    
    Новый цикл for .. of предназначен для итерации по элементам коллекций (итерируемые объекты), но в отличие от цикла for .. in при итерациях используется значение, а не ключ.
    
    while (условие) {...}
    Пока условие приводится к true цикл будет работать.
    
    do {...} while (условие)
    Сначала выполнится тело потом пройдет проверка условия.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,42 +1,92 @@
 function mainFunction() {
-    // Object
+    // Циклы: While, Do while, for, for of, for in
     const user = {
         name: 'Denis',
+        statusAdmin: false,
         age: 22
     };
-
+    const users = [
+        {
+            name: 'Denis',
+            age: 18
+        }, {
+            name: 'Den',
+            age: 20
+        }, {
+            name: 'Roman',
+            age: '22'
+        }, {
+            name: 'Tom',
+            age: '24'
+        }
+    ];
+    let colorPallet = ['Black', 'Blue', 'Green', 'Yellow', 'Pink', 'Purple'];
     let value;
 
-    // .hasOwnProperty('prop') - проверяет на наличие свойства не проверяет на наличие значения
-    if (user.hasOwnProperty('name')) {
-        value = user.name;
-    } else {
-        value = 'false';
-    }
+    // let i = 0;
+    // while (++i < 10) {
+    //     console.log(i)
+    // }
 
-    // || - приобразовывает к true,
-    // && - приобразовывает к false
+    // let i = 10;
+    // while (i--) {
+    //     console.log(i)
+    // }
 
-    value = 1 || 0; // true
-    value = 1 && 0; // false
+    // let i = 0;
+    // do {
+    //     console.log(`${ i } : Action`);
+    // } while (++i < 6);
 
-    if (user.age >= 18 || user.age >= 21) {
-        value = 'Some actions';
-    } else {
-        value = 'No actions';
-    }
+    // for (let i = 0; i < 6; i++) {
+    //     console.log(i)
+    // }
 
-    // Присвоение значения переменной
-    value = user.name || 'Den';
-    value = 1 && 0 && 2 && 3;
+    // let str = 'Hello';
+    // let res = '';
+    // for (let i = 0; i < str.length; i++) {
+    //     res += str[i] + '#'
+    // }
+    // console.log(res)
 
-    if (user.age >= 21 && user.age <= 65) {
-        value = 'Some actions';
-    } else {
-        value = 'No actions';
-    }
+    // for (let i = 0; i < colorPallet.length; i++) {
+    //     colorPallet[i] = colorPallet[i].toUpperCase()
+    // }
+    // value = colorPallet;
 
-    console.log(value);
+    // for (let i = 0; i < 12; i++) {
+    //     if (i < 5) {
+    //         console.log('continue')
+    //     } else if (i === 11) {
+    //         console.log('break');
+    //     }
+    // }
+
+    // for (let i = 0; i < usersObj.length; i++) {
+    //     value = usersObj[i];
+    // }
+
+    // const usersObj = {};
+    //
+    // for (let i = 0; i < users.length; i++) {
+    //     if (users[i].hasOwnProperty('name')) {
+    //         usersObj[users[i].name] = users[i];
+    //     }
+    // }
+
+    // for (let i = 0; i < users.length; i++) {
+    //     users[i].name === 'Denis' ? users[i].name = 'Decepticon' : ''
+    // }
+
+    // for (let key in users) {
+    //     console.log(users[key])
+    // }
+
+    // for (let value of users) {
+    //     console.log(value)
+    // }
+
+    console.log('Раскоментируй условие')
 }
 
 mainFunction();
