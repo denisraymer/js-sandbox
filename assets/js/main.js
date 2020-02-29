bcf776a6578256f487a5377a762a61d7b1464f72
denisraimer <to.mars.66.miles@gmail.com>
2020-02-29 19:31:13 +0300

    Функции
    Function declaration - Объявление Функции. Создается до выполнения любого кода.
    
    Function expression - Функциональное Выражение. Будет создано только в момент, когда интерпретатор дойдёт до данной строки кода.
    
    Функциональное выражение — это объявление функции в контексте какого-либо выражения.
    
    #info
    https://habr.com/ru/post/274139/

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,92 +1,32 @@
-function mainFunction() {
-    // Циклы: While, Do while, for, for of, for in
-    const user = {
-        name: 'Denis',
-        statusAdmin: false,
-        age: 22
-    };
-    const users = [
-        {
-            name: 'Denis',
-            age: 18
-        }, {
-            name: 'Den',
-            age: 20
-        }, {
-            name: 'Roman',
-            age: '22'
-        }, {
-            name: 'Tom',
-            age: '24'
-        }
-    ];
-    let colorPallet = ['Black', 'Blue', 'Green', 'Yellow', 'Pink', 'Purple'];
-    let value;
+// Функции
 
-    // let i = 0;
-    // while (++i < 10) {
-    //     console.log(i)
-    // }
+const user = {
+    name: 'Denis',
+    statusAdmin: false,
+    age: 22
+};
+let colorPallet = ['Black', 'Blue', 'Green', 'Yellow', 'Pink', 'Purple'];
+let value;
 
-    // let i = 10;
-    // while (i--) {
-    //     console.log(i)
-    // }
+// Function declaration
+function userAuthentication(name, status = false) {
+    if (name === 'admin') {
+        return !status
+    }
 
-    // let i = 0;
-    // do {
-    //     console.log(`${ i } : Action`);
-    // } while (++i < 6);
-
-    // for (let i = 0; i < 6; i++) {
-    //     console.log(i)
-    // }
-
-    // let str = 'Hello';
-    // let res = '';
-    // for (let i = 0; i < str.length; i++) {
-    //     res += str[i] + '#'
-    // }
-    // console.log(res)
-
-    // for (let i = 0; i < colorPallet.length; i++) {
-    //     colorPallet[i] = colorPallet[i].toUpperCase()
-    // }
-    // value = colorPallet;
-
-    // for (let i = 0; i < 12; i++) {
-    //     if (i < 5) {
-    //         console.log('continue')
-    //     } else if (i === 11) {
-    //         console.log('break');
-    //     }
-    // }
-
-    // for (let i = 0; i < usersObj.length; i++) {
-    //     value = usersObj[i];
-    // }
-
-    // const usersObj = {};
-    //
-    // for (let i = 0; i < users.length; i++) {
-    //     if (users[i].hasOwnProperty('name')) {
-    //         usersObj[users[i].name] = users[i];
-    //     }
-    // }
-
-    // for (let i = 0; i < users.length; i++) {
-    //     users[i].name === 'Denis' ? users[i].name = 'Decepticon' : ''
-    // }
-
-    // for (let key in users) {
-    //     console.log(users[key])
-    // }
-
-    // for (let value of users) {
-    //     console.log(value)
-    // }
-
-    console.log('Раскоментируй условие')
+    return console.error('Access is denied !')
 }
 
-mainFunction();
+value = userAuthentication('admin');
+
+value !== undefined ? console.log(value) : '';
+
+// Function expression
+const square = function (x) {
+    return x * x
+};
+
+//
+(function (msg) {
+    console.log(msg)
+})('Hello world');
