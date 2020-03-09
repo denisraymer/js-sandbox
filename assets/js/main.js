b07b31233f6b16a22b7a0cc49828cbd2897a234d
denisraimer <to.mars.66.miles@gmail.com>
2020-03-09 23:05:23 +0300

    Замыкание
    Замыкания — это функции, ссылающиеся на независимые (свободные) переменные. Другими словами, функция, определённая в замыкании, «запоминает» окружение, в котором она была создана.
    
    Существует три типа кода в ECMAScript:
    - глобальный код ( window и все его свойства и методы );
    - код функции ( function foo() { ...some code... } );
    - eval код ( eval(‘1 + 1’) ).
    
    Каждый тип кода выполняется в своем контексте исполнения (execution context). Существует только один глобальный контекст, и может быть множество экземпляров контекстов функции.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,135 +1,112 @@
 const mainFunction = function () {
-    // Перебирающие методы массивов
-    // forEach
-    // sort
-    // map
-    // filter
-    // some/every
-    // reduce
-    // find
-
-    const users = [
-        {
-            _id: "5cdce6ce338171bb473d2855",
-            index: 0,
-            isActive: false,
-            balance: 2397.64,
-            age: 39,
-            name: "Lucile Finley",
-            gender: "female",
-            company: "ZOXY",
-            email: "lucilefinley@zoxy.com",
-            phone: "+1 (842) 566-3328",
-            registered: "2015-07-12T09:39:03 -03:00"
-        },
-        {
-            _id: "5cdce6ce0aa8d071fa4f4cc5",
-            index: 1,
-            isActive: true,
-            balance: 2608.48,
-            age: 33,
-            name: "Woodward Grimes",
-            gender: "male",
-            company: "FORTEAN",
-            email: "woodwardgrimes@fortean.com",
-            phone: "+1 (960) 436-3138",
-            registered: "2014-09-08T03:24:39 -03:00"
-        },
-        {
-            _id: "5cdce6ce103de120d32d6fe4",
-            index: 2,
-            isActive: true,
-            balance: 1699.99,
-            age: 25,
-            name: "Robinson Coleman",
-            gender: "male",
-            company: "GENMOM",
-            email: "robinsoncoleman@genmom.com",
-            phone: "+1 (852) 543-3171",
-            registered: "2019-04-23T08:24:58 -03:00"
-        },
-        {
-            _id: "5cdce6cebada7a418d8ccb3d",
-            index: 3,
-            isActive: true,
-            balance: 2621.84,
-            age: 25,
-            name: "Austin Benton",
-            gender: "male",
-            company: "ZILIDIUM",
-            email: "austinbenton@zilidium.com",
-            phone: "+1 (977) 573-2627",
-            registered: "2016-08-02T10:08:24 -03:00"
-        },
-        {
-            _id: "5cdce6ced81fe99596d9cef5",
-            index: 4,
-            isActive: true,
-            balance: 1297.31,
-            age: 37,
-            name: "Casandra Stout",
-            gender: "female",
-            company: "ANACHO",
-            email: "casandrastout@anacho.com",
-            phone: "+1 (929) 465-3804",
-            registered: "2018-04-14T11:27:26 -03:00"
-        },
-        {
-            _id: "5cdce6ce6c3ae6c4d6f39e88",
-            index: 5,
-            isActive: false,
-            balance: 2165.49,
-            age: 20,
-            name: "Valencia Carrillo",
-            gender: "male",
-            company: "XEREX",
-            email: "valenciacarrillo@xerex.com",
-            phone: "+1 (977) 522-3378",
-            registered: "2014-02-14T11:45:27 -02:00"
-        }
-    ];
+    // Замыкание
     let value;
 
-    // forEach
-    users.forEach((user, i, arr) => {
-        // console.log(user, i, arr)
-    });
+    function getFullName() {
+        return function (firstName, lastName) {
+            return `${firstName} ${lastName}`
+        }
+    }
 
-    // filter
-    value = users.filter((user) => user.age < 30);
-    value = users.filter((user) => user.isActive);
+    value = getFullName()('Denis', 'Raymer');
 
-    // map
-    value = users.map(user => user.registered);
-    value = users.map((user) => ({
-        name: user.name,
-        registered: user.registered
-    }));
 
-    // reduce
-    value = users.reduce((acc, user) => acc + user.balance, 0);
-    // Формирование объекта объектов
-    value = users.reduce((acc, user) => {
-        acc[user._id] = user;
-        return acc
-    }, {});
+    function updateValue(val = 0) {
+        let x = val;
+        return function (num = 0) {
+            return x += num
+        }
+    }
 
-    // some/every
-    // some - если хоть один true
-    value = users.some(user => user.gender === 'male');
-    // every
-    // every - если все true
-    value = users.every(user => user.age > 18);
+    const updateValueOne = updateValue(2);
+    value = updateValueOne(1);
 
-    // find
-    value = users.find(user => user.name === 'Lucile Finley');
+    value = updateValue(2)(2);
 
-    // sort
-    const strArr = ['Denis', 'Bill', 'Alina'];
-    value = strArr.sort();
 
-    const numArr = [10, 7, 44, 32];
-    value = numArr.sort((prev, next) => prev - next);
+    function checkCred () {
+        const login = 'test';
+        const password = '123456';
+
+        return {
+            checkLogin(value) {
+                return login === value
+            },
+            loginPassword(value) {
+                return password === value;
+            }
+        }
+    }
+
+    const checkOgj = checkCred(); // return obj
+
+    value = checkOgj.checkLogin('test');
+    value = checkOgj.loginPassword('123456');
+
+
+    function closureExample() {
+        const arrOfFunc = [];
+        let value = '';
+
+        for (let i = 0; i < 10; i++) {
+            value += i;
+            arrOfFunc.push(function () {
+                return `${ value } ${ i }`
+            })
+        }
+
+        return arrOfFunc;
+    }
+
+    value = closureExample();
+    value = value[2]();
+
+
+    // DZ
+    function minus(value = 0) {
+        return function (num = 0) {
+            return value - num
+        }
+    }
+    value = minus(10)(6);
+
+    function multiplyMaker(val = 0) {
+        let middleValue = val;
+        return function (num = 0) {
+            return middleValue *= num;
+        }
+    }
+
+    let multiply = multiplyMaker(2);
+    value = multiply(2);
+    value = multiply(1);
+    value = multiply(3);
+    value = multiply(10);
+
+    function creatingString() {
+        let arrString = [];
+
+        return {
+            getStrRevers(index = 0) {
+                value = arrString[index].split('').reverse().join('');
+                return this
+            },
+            getStrLength(index = 0) {
+                value = arrString[index].length;
+                return this
+            },
+            createStr(val = '') {
+                arrString.push(val.toString());
+                return this
+            },
+            getStr(index = 0) {
+                value = arrString[index];
+                return this
+            }
+        }
+    }
+
+    creatingString().createStr(123).getStr().getStrLength().getStrRevers();
 
     console.log(value)
 };
