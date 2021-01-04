91ee702aac52e2a24cba5c8855bb5dc262c14ecf
denisraymer <to.mars.66.miles@gmail.com>
2021-01-04 23:15:34 +0300

    Работа прототипов и контекста в связке (Пример с функцией умножения)

--- assets/js/lessons/Advanced/context.js
+++ assets/js/lessons/Advanced/context.js
@@ -7,16 +7,16 @@
 // Метод apply Необходим для привязки контекста к объекту (Сразу вызывает функцию параметры передаются в массиве)
 
 function hello() {
-    console.log('Hello', this);
+    console.log('\n Hello', this);
 }
 
 const person = {
     name: 'GoLang',
     age: 16,
     sayHello: hello,
-    sayHelloWindow: hello.bind(window),
+    // sayHelloWindow: hello.bind(window),
     logInfo: function (job, phone) {
-        console.group(`${this.name} info:`);
+        console.group(`\n ${this.name} info:`);
         console.log(`Name is ${this.name}`);
         console.log(`Age is ${this.age}`);
         console.log(`Phone is ${phone}`);
@@ -35,9 +35,32 @@ hello(); // Указывает на объект Window
 person.sayHello(); // Указывает на объект person
 person.logInfo('BackEnd', '8-532-874-71-05'); // Указывает на объект person
 
-// person.logInfo.bind(lena)() // Указывает на объект lena
+person.logInfo.bind(lena)() // Указывает на объект lena
 
 const fnPersonInfo = person.logInfo.bind(lena, 'FrontEnd', '8-902-354-21-35'); // Указывает на объект lena
 fnPersonInfo();
 
+// ============
+// Работа прототипов и контекста в связке на примере,
+// функции умножающей значение массива на переданное в неё число
 
+const arrayMultBy = [1, 2, 3, 4, 5, 6];
+
+// Пример без использования прототипов
+function multBy(arr, n) {
+    return arr.map(function (i) {
+        return i * n
+    });
+}
+
+// Пример с использование прототипов
+Array.prototype.multBy = function (n) {
+    return this.map(function (i) {
+        return i * n
+    });
+}
+
+console.group('\n MultBy function: ');
+console.log(multBy(arrayMultBy, 15));
+console.log(arrayMultBy.multBy(12));
+console.groupEnd();
