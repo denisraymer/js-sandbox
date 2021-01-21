cbc503109369885d35f7c98d230f2ea350c48a87
denisraymer <to.mars.66.miles@gmail.com>
2021-01-21 19:49:28 +0300

    Promise - используется для отложенных и асинхронных вычислений.
    executor - Объект функции с двумя аргументами resolve и reject. Функция executor  получает оба аргумента и выполняется сразу, еще до того как конструктор вернет созданный объект. Первый аргумент (resolve) вызывает успешное исполнение промиса, второй (reject) отклоняет его. Обычно функция executor описывает выполнение какой-то асинхронной работы, по завершении которой необходимо вызвать функцию resolve или reject. Обратите внимание, что возвращаемое значение функции executor игнорируется.

---
+++ assets/js/lessons/Advanced/promise.js
@@ -0,0 +1,50 @@
+const promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        const backendData = {
+            server: 'AWS',
+            port: 2000,
+            status: 'Working'
+        }
+        resolve(backendData);
+    }, 2000);
+});
+
+promise.then(function (data) {
+    data.modified = true;
+    return data;
+}).then(function (clientData) {
+    clientData.fromPromise = true;
+    return clientData;
+}).catch(function (error) {
+    console.log('Error: ', error);
+}).finally(function () {
+    console.log('Finally');
+});
+
+const promiseSleep = function (time) {
+    return new Promise(resolve => {
+        setTimeout(() => {
+            const backendData = {
+                server: 'AWS',
+                port: 2000,
+                status: 'Working'
+            };
+
+            resolve(backendData);
+        }, time);
+    });
+};
+
+promiseSleep(2000).then(data => console.log('Promises slip:', data));
+
+
+// Метод all ожидает выполнения всех переданных в него функций
+// и после этого выдает результат
+Promise.all([promiseSleep(2000), promiseSleep(5000)]).then(() => {
+    console.log('All promises');
+});
+
+// Метод race последовательно выполняет переданные в неё функции
+Promise.race([promiseSleep(2000), promiseSleep(5000)]).then(() => {
+    console.log('Race promises');
+})
