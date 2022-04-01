703ec9f1a6f7041929a436d51f97e934880566ff
DenisRaymer <to.mars.66.miles@gmail.com>
2022-04-01 19:34:37 +0300

    fea: Added materials on the node js course from "learn.js"

---
+++ src/NodeJS/server.js
@@ -0,0 +1,17 @@
+const db = require('./db');
+const User = require('./user');
+const log = require('./logger')(module);
+
+function run() {
+  const vasya = new User('Вася');
+  const petya = new User('Петя');
+
+  vasya.hello(petya)
+  log(db.getPhrases('Run successful'));
+}
+
+if (module.parent) {
+  module.exports = run;
+} else {
+  run();
+}
