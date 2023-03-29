7acca71448bbf3016ed15ab136cd62bd51944ae0
DenisRaymer <to.mars.66.miles@gmail.com>
2023-03-29 16:14:57 +0600

    fea: Пример работы абстракции и композиции

---
+++ src/JS/OOP/ulbi/InteractionBetweenClasses/index.ts
@@ -0,0 +1,8 @@
+import { Car } from "./_Car"
+import { Freshener } from "./_Freshener"
+
+const hondaCRV = new Car()
+hondaCRV.drive()
+
+const hondaOdyssey = new Car(Freshener)
+hondaOdyssey.drive()
